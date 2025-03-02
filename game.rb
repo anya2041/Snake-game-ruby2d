873d7592536e7645d0fc924414d02349c82d58b5
Anya 2022088 <127613201+anya2041@users.noreply.github.com>
2025-03-02 21:52:41 +0530

    added code

--- game.rb
+++ game.rb
@@ -91,3 +91,32 @@ class Snake
           Square.new(x: @ball_x * SQUARE_SIZE, y: @ball_y * SQUARE_SIZE, size: SQUARE_SIZE, color: 'yellow')
           Text.new(text_message, color: 'green', x: 10, y: 10, size: 25, z: 1)
         end
+
+        def snake_hit_ball?(x, y)
+            @ball_x == x && @ball_y == y
+          end
+        
+          def record_hit
+            @score += 1
+            @ball_x = rand(Window.width / SQUARE_SIZE)
+            @ball_y = rand(Window.height / SQUARE_SIZE)
+          end
+        
+          def finish
+            @finished = true
+          end
+        
+          def finished?
+            @finished
+          end
+        
+          private
+        
+          def text_message
+            if finished?
+              "Game over, Your Score was #{@score}. Press 'R' to restart. "
+            else
+              "Score: #{@score}"
+            end
+          end
+        end
